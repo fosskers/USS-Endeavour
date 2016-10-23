bf213db83d03591596ac5b216079ba9917c149df
Colin Woodbury <colingw@gmail.com>
2016-10-22 17:04:14 -0700

    Checkpoint
    - Uses `optparse-generic` for extremely simple CLI arg generation.

--- Endeavour.hs
+++ Endeavour.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+--import Servant.API
+import Options.Generic
+import Endeavour.Genetics
+
+---
+
+-- | Command-line arguments.
+data Args = Args { config :: FilePath } deriving (Generic)
+
+instance ParseRecord Args
+
 main :: IO ()
-main = putStrLn "U.S.S. Endeavour - Computing Core activated."
+main = do
+  Args c <- getRecord "U.S.S. Endeavour - Computing Core"
+  env <- genes c
+  putStrLn c
+  putStrLn "U.S.S. Endeavour - Computing Core activated."
