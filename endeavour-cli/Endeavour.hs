5e825ed48613786e54f1975608167fb4861ff8fa
Colin Woodbury <colingw@gmail.com>
2017-01-05 09:24:54 -0800

    (cli) Simple event handling

--- endeavour-cli/Endeavour.hs
+++ endeavour-cli/Endeavour.hs
@@ -10,7 +10,10 @@ import           Brick.Widgets.Border.Style
 import           Brick.Widgets.Center
 import           Brick.Widgets.ProgressBar
 import           Control.Monad (void)
+import           Data.Foldable (toList)
+import           Data.List (intersperse)
 import qualified Data.Text as T
+import qualified Deque as D
 import qualified Graphics.Vty as V
 import           Lens.Micro
 import           Lens.Micro.TH
@@ -19,8 +22,11 @@ import           Text.Printf.TH
 
 ---
 
+-- | Possible application pages.
+data Page = Lights | Media deriving (Eq, Enum, Show)
+
 -- | The application state.
-data System = System { _msg :: T.Text } deriving (Eq, Show)
+data System = System { _msg :: T.Text, _pages :: D.Deque Page } deriving (Eq, Show)
 makeLenses ''System
 
 -- | All resource names.
@@ -30,19 +36,36 @@ boxy :: Widget n
 boxy = withBorderStyle unicodeRounded . border . padAll 5 $ txt "Welcome, Officer."
 
 widgets :: System -> Widget n
-widgets s = vBox [ centerWith (Just '.') boxy
-                 , padRight Max . padLeft (Pad 1) . txt $ _msg s --progressBar (Just "Progress") 0.4
-                 ]
+widgets s = centerWith (Just '.') boxy <=> footer s
+
+footer :: System -> Widget n
+footer s = hBox
+  [ padRight Max . padLeft (Pad 1) . txt $ _msg s --progressBar (Just "Progress") 0.4
+  , padLeft Max . padRight (Pad 1) . txt . mconcat . intersperse " | " . map (T.pack . show) . toList $ _pages s
+  ]
 
 -- | The final conglomeration of `Widget`s.
 ui :: System -> Widget n
 ui s = withBorderStyle unicodeBold . borderWithLabel (padLeftRight 1 $ txt "Endeavour System Controls") $ widgets s
 
+-- | The application event handler. Resizing still happens automatically.
+handle :: System -> BrickEvent t t1 -> EventM n (Next System)
+handle s (VtyEvent (V.EvKey V.KEsc _)) = halt s
+handle s (VtyEvent (V.EvKey (V.KChar c) _)) = continue (s & msg .~ [st|Key: %c|] c)
+handle s (VtyEvent (V.EvKey V.KLeft [V.MShift])) = continue (s & msg .~ "<==" & pages %~ D.shiftRight)
+handle s (VtyEvent (V.EvKey V.KLeft _)) = continue (s & msg .~ "<--")
+handle s (VtyEvent (V.EvKey V.KRight [V.MShift])) = continue (s & msg .~ "==>" & pages %~ D.shiftLeft)
+handle s (VtyEvent (V.EvKey V.KRight _)) = continue (s & msg .~ "-->")
+handle s (VtyEvent (V.EvKey V.KDown _)) = continue (s & msg .~ "vvv")
+handle s (VtyEvent (V.EvKey V.KUp _)) = continue (s & msg .~ "^^^")
+handle s (VtyEvent (V.EvKey V.KEnter _)) = continue (s & msg .~ "ENTER")
+handle s _ = continue (s & msg .~ "Warning: Uncaught VtyEvent")
+
 -- | A description of how to run our application.
 app :: App System () Name
 app = App { appDraw = \s -> [ui s]
           , appChooseCursor = neverShowCursor
-          , appHandleEvent = resizeOrQuit
+          , appHandleEvent = handle
           , appStartEvent = pure
           , appAttrMap = const $ attrMap V.defAttr [ (progressCompleteAttr, bg V.blue) ]
           }
@@ -50,4 +73,7 @@ app = App { appDraw = \s -> [ui s]
 main :: IO ()
 main = do
   user <- T.pack <$> getEffectiveUserName
-  void . defaultMain app . System $ [st|Hello, %s.|] (T.toTitle user)
+  let m = [st|Hello, %s.|] (T.toTitle user)
+      p = D.fromList [Lights ..]
+  void . defaultMain app $ System m p
+  putStrLn "Shutdown complete."
