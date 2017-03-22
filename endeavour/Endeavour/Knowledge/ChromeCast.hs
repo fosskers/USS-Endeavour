e59b5dd9398658dc1b23f3855702150eb83c1626
Colin Woodbury <colingw@gmail.com>
2017-03-21 20:32:28 -0700

    New `Media` type for casting files
    - This should make it easier to wield groups of filenames on the Media
      page of the Endeavour Console.

--- endeavour/Endeavour/Knowledge/ChromeCast.hs
+++ endeavour/Endeavour/Knowledge/ChromeCast.hs
@@ -11,15 +11,18 @@
 -- Functions for interacting with a ChromeCast.
 
 module Endeavour.Knowledge.ChromeCast
-  ( -- * Commands
-    cast, pause, unpause, stop
+  ( -- * Types
+    Media(..)
+  -- * Commands
+  , cast, pause, unpause, stop
     -- * Resources
-  , audio, video
+  , albums, videos
   ) where
 
 import           Control.Monad (void)
+import           Data.List
 import           Data.Monoid
-import           Data.Text (Text, breakOnEnd)
+import qualified Data.Text as T
 import           Endeavour.Genetics
 import           Endeavour.Memory
 import           Lens.Micro
@@ -28,20 +31,23 @@ import qualified Text.Fuzzy as F
 
 ---
 
+-- | An audio album with its name and tracklist, or a video file with its path.
+data Media = Album T.Text [T.Text] | Video T.Text
+
 -- | ChromeCast can handle MP4, MKV, and normal audio formats (including FLAC)
 -- natively. AVIs will be converted on-the-fly via the @-transcode@ flag.
 --
 -- @stream2chromecast@ will exit successfully when the file is done playing,
 -- or when another "sender app" takes over the ChromeCast.
-cast :: ERL r => Text -> Eff r ()
+cast :: ERL r => T.Text -> Eff r ()
 cast f = do
   toCast <- fileToCast f
   chronicle Info $ "Casting " <> toCast
   sheff "Failed to stream to ChromeCast." $ run_ "stream2chromecast" (castArgs toCast)
 
 -- | If the media file is an AVI, we need to transcode it.
-castArgs :: Text -> [Text]
-castArgs f = case breakOnEnd "." f of
+castArgs :: T.Text -> [T.Text]
+castArgs f = case T.breakOnEnd "." f of
   (_, "avi") -> ["-transcode", f]
   _ -> [f]
 
@@ -61,7 +67,7 @@ stop = do
   sheff "Couldn't stop the Chromecast." $ run_ "stream2chromecast" ["-stop"]
 
 -- | Silently and asynchronously run a Shell command.
-sheff :: ERL r => Text -> Sh a -> Eff r ()
+sheff :: ERL r => T.Text -> Sh a -> Eff r ()
 sheff t s = void . effShelly t . asyncSh . print_stderr False $ print_stdout False s
 
 -- | Execute a Shelly command, smothering any thrown exceptions in `Maybe`.
@@ -69,24 +75,34 @@ maybeSh :: Sh a -> IO (Maybe a)
 maybeSh s = catchany (Just <$> shelly s) (\_ -> pure Nothing)
 
 -- | Catch any Shelly exception and rethrow with a given message.
-effShelly :: ERL r => Text -> Sh a -> Eff r a
+effShelly :: ERL r => T.Text -> Sh a -> Eff r a
 effShelly e s = send (maybeSh s) >>= maybe (throwError e) pure
 
 -- | Given some suggestion text, finds a suitable media file to cast,
 -- if it can.
-fileToCast :: ERL r => Text -> Eff r Text
+fileToCast :: ERL r => T.Text -> Eff r T.Text
 fileToCast t = ((++) <$> audio <*> video) >>= liftEither . fileToCast' t
 
 -- | Given its own fuction to isolate pure code, as well as hand-hold EE.
-fileToCast' :: Text -> [Text] -> Either Text Text
+fileToCast' :: T.Text -> [T.Text] -> Either T.Text T.Text
 fileToCast' t fs = maybe (Left "No suitable media file found.") Right (F.simpleFilter t fs ^? _head)
 
--- | All audio files. Assumes that each file in `audio/` is a directory,
--- and that each directory has no further subdirectories.
-audio :: RL r => Eff r [Text]
+-- | All audio files.
+audio :: RL r => Eff r [T.Text]
 audio = asks _media >>= send . shelly @IO . f
-  where f p = concat <$> (ls (p </> ("audio" :: Text)) >>= mapM lsT)
+  where f p = concat <$> (ls (p </> ("audio" :: T.Text)) >>= mapM lsT)
+
+-- | Every album with its tracklist. Assumes that each file in @audio/@ is a
+-- directory, and that each directory has no further subdirectories.
+albums :: RL r => Eff r [Media]
+albums = f <$> asks _media <*> audio
+  where f fp fs = map h . groupBy (\(a,_) (b,_) -> a == b) . sort $ map (g fp) fs
+        g fp t = maybe ("",t) (T.breakOn "/") $ T.stripPrefix (fp <> "audio/") t
+        h ps = Album (fst $ head ps) $ map snd ps
+
+video :: RL r => Eff r [T.Text]
+video = asks _media >>= send . shelly @IO . lsT . (</> ("video" :: T.Text))
 
 -- | All video files.
-video :: RL r => Eff r [Text]
-video = asks _media >>= send . shelly @IO . lsT . (</> ("video" :: Text))
+videos :: RL r => Eff r [Media]
+videos = map Video . sort <$> video
